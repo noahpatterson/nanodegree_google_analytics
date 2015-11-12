894e90e2c0dfa3f95d1972fcad1ef4465bf6f408
Noah Patterson <noah@noahpatterson.me>
2015-11-12 07:48:53 -0500

    working show all dinner button

--- app/src/main/java/com/example/android/dinnerapp/AllRecipesActivity.java
+++ app/src/main/java/com/example/android/dinnerapp/AllRecipesActivity.java
@@ -1,12 +1,16 @@
 package com.example.android.dinnerapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class AllRecipesActivity extends AppCompatActivity {
 
@@ -16,8 +20,20 @@ public class AllRecipesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_all_receipes);
 
         String [] allDinners = getIntent().getStringArrayExtra("allDinners");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.all_recipes_list_item, allDinners);
 
         ListView listView = (ListView) findViewById(R.id.all_recipes_listView);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String dinner =(String) parent.getItemAtPosition(position);
+                Intent intent = new Intent(getBaseContext(), OrderDinnerActivity.class);
+                intent.putExtra("selectedDinner", dinner);
+                startActivity(intent);
+            }
+        });
+
+        listView.setAdapter(arrayAdapter);
 
     }
 }
