e3227d3af128ffd854be1623e3ef13f4c5f1378c
Noah Patterson <noah@noahpatterson.me>
2015-11-16 07:50:49 -0500

    start ecommerce steps

--- app/src/main/java/com/example/android/dinnerapp/OrderDinnerActivity.java
+++ app/src/main/java/com/example/android/dinnerapp/OrderDinnerActivity.java
@@ -18,16 +18,25 @@ package com.example.android.dinnerapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
+import com.google.android.gms.analytics.ecommerce.Product;
+import com.google.android.gms.analytics.ecommerce.ProductAction;
 
 
 public class OrderDinnerActivity extends Activity {
-    String selectedDinnerExtrasKey = String.valueOf(R.string.selected_dinner);
+    //String selectedDinnerExtrasKey = String.valueOf(R.string.selected_dinner);
+    private String selectedDinner;
+    private Product product;
+    private Tracker gaTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.show_info);
+        setContentView(R.layout.order_dinner);
     }
 
     protected void onStart() {
@@ -40,9 +49,50 @@ public class OrderDinnerActivity extends Activity {
         // Set the text
         TextView tv = (TextView) findViewById(R.id.textView_info);
 
-        String dinner = getIntent().getStringExtra(selectedDinnerExtrasKey);
+        String dinner = getIntent().getStringExtra("selectedDinner");
+        selectedDinner = dinner;
         tv.setText("This is where you will order the selected dinner: \n\n" +
                 dinner);
+        sendViewProductHit(dinner);
+    }
+
+    private void sendViewProductHit(String dinner) {
+        String dinnerId = Utility.getDinnerId(dinner);
+        product = new Product()
+                .setId(dinnerId)
+                .setName("dinner")
+                .setPrice(5)
+                .setVariant(dinner)
+                .setQuantity(1);
+
+        ProductAction productAction = new ProductAction(ProductAction.ACTION_DETAIL);
+
+        gaTracker = ((MyApplication) getApplication()).getTracker();
+
+        gaTracker.send(new HitBuilders.EventBuilder()
+                .setCategory("shopping steps")
+                .setAction("View order dinner screen")
+                .setLabel(dinner)
+                .addProduct(product)
+                .setProductAction(productAction)
+                .build());
+    }
+
+    private void sendAddDintterToCartHit() {
+        ProductAction productAction = new ProductAction(ProductAction.ACTION_ADD);
+        gaTracker.send(new HitBuilders.EventBuilder()
+                .setCategory("shopping steps")
+                .setAction("add dinner to cart")
+                .setLabel(selectedDinner)
+                .addProduct(product)
+                .setProductAction(productAction)
+                .build());
+    }
+
+    public void addDinnerToCart(View view) {
+        sendAddDintterToCartHit();
+        Utility.showMyToast("added " + selectedDinner + " to cart",this);
+        return;
     }
 
 }
